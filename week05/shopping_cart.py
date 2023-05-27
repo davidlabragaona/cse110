e68e423ae492bf1e7e56bf0fa7b1270e5b0cf0a6
David Labra Gaona <davidlabragaona@gmail.com>
2023-05-27 19:03:29 -0300

    life expectancy exercise

--- week05/shopping_cart.py
+++ week05/shopping_cart.py
@@ -1,3 +1,5 @@
+#Creativity: validation of numeric option and price values
+
 # Author: David Labra Gaona
 # Purpose: Shopping Cart Assignement
 
@@ -13,34 +15,53 @@ item = ""
 price = 0.00
 shopping_list = []
 price_list = []
+total = 0.00
 
 while option != 5:
     print("\nPlease select one of the following: ")
     for i, item in enumerate(options):
         print(f"{i + 1}. {item}")
 
-    option = int(input("\nPlease, enter an action: "))
+    option = input("\nPlease, enter an action: ")
+    if option.isnumeric():
+        option = int(option)
+    else:
+        print("Invalid option")
+        continue
     # Add item
     if option == 1:
         item = input("What item would you like to add? ")
-        shopping_list.append(item)
-        price = float(input(f"What is the price of {item}? "))
-        price_list.append(price)
-        print(f"'{item}' has been added to the cart.")
+        price = input(f"What is the price of {item}? ")
+        if price.isnumeric():
+            price = float(price)
+            shopping_list.append(item)
+            price_list.append(price)
+            print(f"'{item}' has been added to the cart.")
+        else:
+            print("Invalid price.")
     # View cart
     elif option == 2:
         print("The contents of the shopping cart are: ")
         for i, item in enumerate(shopping_list):
-            print(f"{i+1}. {item} - ${price_list[i]:.2f}")
+            print(f"\t{i+1}. {item} - ${price_list[i]:.2f}")
     elif option == 3:
-        i = int(input("Which item would you like to remove? "))
-        if (i - 1) > len(shopping_list) and len(shopping_list) > 0:
+        i = input("Which item would you like to remove? ")
+        if i.isnumeric:
+            i = int(i)
+        else:
+            print("Invalid option")
+            continue
+        if (i - 1) > len(shopping_list) or len(shopping_list) == 0:
             print("Sorry that is not a valid item number.")
         else:
             shopping_list.pop(i - 1)
             price_list.pop(i - 1)
             print("Item removed")
+    elif option == 4:
+        total = 0
+        for p in price_list:
+            total += p
+        print(f"The total price of the items in the shopping cart is ${total:0.2f}")
     # quit
     elif option == 5:
         print("Thank you. Goodbye.")
-
--- week06/life_expectancy.py
+++ week06/life_expectancy.py
@@ -9,15 +9,30 @@ highest_life_expectancy_country = ""
 highest_life_expectancy_year = 0
 highest_life_expectancy = 0
 
+year = 0
+year_user = 0
+year_cases = []
+average = 0.00
+counter = 0
+countries = []
+
 with open("life-expectancy.csv", ) as dataset:
+
+  year_user = input("Enter the year of interest: ")
+  if not year_user.isnumeric:
+    print("Invalid option")
+    print()
+    exit(1)
+  else:
+    year_user = int(year_user)
+    print()
+
   for i, line in enumerate(dataset):
-    
-    #We skeep the first line because it's the header
+    #We skip the first line because it's the header
     if i == 0:
       continue
 
     line = line.strip().split(",")
-    #print(line)
 
     country_name = line[0]
     year = int(line[2])
@@ -33,5 +48,35 @@ with open("life-expectancy.csv", ) as dataset:
       highest_life_expectancy_year = year
       highest_life_expectancy = life_expectancy
 
-print(f"Lowest life expectancy: {lowest_life_expectancy}")
-print(f"Higest life expectancy: {highest_life_expectancy}")
+    if year == year_user:
+      year_cases.append(line)
+    
+print(f"The overall max life expectancy is: {highest_life_expectancy:.2f} from {highest_life_expectancy_country} in {highest_life_expectancy_year}")
+print(f"The overall min life expectancy is: {lowest_life_expectancy:.2f} from {lowest_life_expectancy_country} in {lowest_life_expectancy_year}")
+print()
+
+highest_life_expectancy = 0
+lowest_life_expectancy = 99999.9
+for line in year_cases:
+  country_name = line[0]
+  life_expectancy = float(line[3])
+  average += life_expectancy
+
+  if life_expectancy < lowest_life_expectancy:
+    lowest_life_expectancy_country = country_name
+    lowest_life_expectancy = life_expectancy
+  
+  if life_expectancy > highest_life_expectancy:
+    highest_life_expectancy_country = country_name
+    highest_life_expectancy = life_expectancy
+
+average /= len(year_cases)
+print(f"For the year {year_user}:")
+print(f"The average life expectancy across all countries was {average:.2f}")
+print(f"The max life expectancy was in {highest_life_expectancy_country:.2f} with {highest_life_expectancy:.2f}")
+print(f"The min life expectancy was in {lowest_life_expectancy_country:.2f} with {lowest_life_expectancy:.2f}")
+print()
+
+#Creativity
+country = input("Enter the name of a country: ")
+
